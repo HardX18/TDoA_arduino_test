d8ae9f0084a1e02b6e81cbf0f1081b70114cf8aa
HardX18 <159409467+HardX18@users.noreply.github.com>
2024-04-19 23:40:06 +0530

    Add files via upload

---
+++ tdoa_Engine/tdoaEngine.ino
@@ -0,0 +1,16 @@
+#include "clockCorrectionEngine.c"
+#include "tdoaEngine.c"
+#include "tdoaEngineInstance.c"
+#include "tdoaStats.c"
+#include "tdoaStorage.c"
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ tdoa_Engine/tdoaEngineInstance.h
@@ -0,0 +1,8 @@
+/* tdoaEngineInstance.h - exposes the tdoa engine state instance
+ */
+
+#pragma once
+
+#include "tdoaEngine.h"
+
+extern tdoaEngineState_t tdoaEngineState;
